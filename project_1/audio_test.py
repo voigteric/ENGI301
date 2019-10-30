efbde2ea511eb7cf0d88189b7c202a4d7f5c5c90
eric-voigt <ericvoigt@rice.edu>
2019-10-29 22:31:54 -0500

    project work update

--- project_1/audio_test.py
+++ project_1/audio_test.py
@@ -1,6 +1,6 @@
 """
 --------------------------------------------------------------------------
-Synth Glove Test Setup with Potentiometers
+Synth Glove Audio Test
 --------------------------------------------------------------------------
 License:   
 Copyright 2019 - Eric Voigt
@@ -30,88 +30,45 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 --------------------------------------------------------------------------
-Modulate sound output with potentiometer input
-
-  - Potentiometer signals connected to AIN0, AIN1, AIN2, AIN3, AIN4
-  - USB audio adapter connected to USB1 (P1_5, P1_7, P1_9, P1_11, P1_13, P1_15)
-
-When potentiometers are changed, the audio output will change
+Call and play audio files with python
 
 --------------------------------------------------------------------------
 Background:
-  - Analog servo control class demonstration (analog_servo_control.py)
-  - PocketBeagle audio driver setup (http://einsteiniumstudios.com/make-your-beaglebone-speak.html)
+  - Class notes on audio packages
 
 """
 # ------------------------------------------------------------------------
 # Packages
 # ------------------------------------------------------------------------
 
-import time
-import Adafruit_BBIO.ADC as ADC
 import os
+import time
 
 # ------------------------------------------------------------------------
 # Global Variables
 # ------------------------------------------------------------------------
 
-debug       = True
 
-update_time = 0.025
-
-# ------------------------------------------------------------------------
-# Pin Assignments
-# ------------------------------------------------------------------------
-
-# Analog setup for flex sensing
-A_THUMB   = "P1_27"                      # AIN_4
-A_INDEX   = "P1_25"                      # AIN_3
-A_MIDDLE  = "P1_23"                      # AIN_2
-A_RING    = "P1_21"                      # AIN_1
-A_PINKY   = "P1_19"                      # AIN_0
 
 # ------------------------------------------------------------------------
 # Functions
 # ------------------------------------------------------------------------
 
-def setup():
-    """set up the hardware components"""
-    ADC.setup()
 
-# end def
 
-def play_sounds(thumb_flex, thumb_prev, index_flex, index_prev, middle_flex, middle_prev, ring_flex, ring_prev, pinky_flex, pinky_prev):
-    """check to see change in finger flex. if so, play associated sound"""
 
 # ------------------------------------------------------------------------
 # Main script
 # ------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    setup()
-    print("Synth Glove Testbed Program Start")
+    print("Audio Test Program Start")
 
     while True:
-        """scale and print the potentiometer signals for testing"""
-        t_f = float(ADC.read(A_THUMB))
-        i_f = float(ADC.read(A_INDEX))
-        m_f = float(ADC.read(A_MIDDLE))
-        r_f = float(ADC.read(A_RING))
-        p_f = float(ADC.read(A_PINKY))
-        
-        """debugging display"""
-        if (debug):
-            print("thumb = {0}; index = {1}; middle = {2}; ring = {3}; pinky = {4}".format(t_f, i_f, m_f, r_f, p_f))
-        
-        play_sounds(t_f, t_p, i_f, i_p, m_f, m_p, r_f, r_p, p_f, p_p)
-        
-        """store previous values for next step"""
-        t_p = t_f
-        i_p = i_f
-        m_p = m_f
-        r_p = r_f
-        p_p = p_f
-        
-        time.sleep(update_time)
+        os.system("aplay /var/lib/cloud9/ENGI301/project_01/808.wav")
+        os.system("aplay /var/lib/cloud9/ENGI301/project_01/hand_clap.wav")
+        os.system("aplay /var/lib/cloud9/ENGI301/project_01/hi_hat.wav")
+        os.system("aplay /var/lib/cloud9/ENGI301/project_01/snare.wav")
+        os.system("aplay /var/lib/cloud9/ENGI301/project_01/tom.wav")
 
-    print("Synth Glove Testbed Program Finished")
+    print("Audio Test Program Finished")
